75e8e44abfef1b0aa8591c1bf3b988b04a96beeb
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-04 20:10:43 +0530

    Refactor TripPage component: move DetailedItineraryCard to app directory and remove unused components

--- src/app/tripPage/page.tsx
+++ src/app/tripPage/page.tsx
@@ -1,14 +1,10 @@
-import DetailedItineraryCard from "@/components/DetailedItineraryCard";
-import SafetyCard from "@/components/SafetyCard";
-import CancellationCard from "@/components/CancellationCard";
+import DetailedItineraryCard from "@/app/tripPage/components/DetailedItineraryCard";
 
 export default function TripPage(){
   return (
     <section
     className="w-full min-h-svh p-6 flex flex-col gap-4">
       <DetailedItineraryCard />
-      <SafetyCard />
-      <CancellationCard />
     </section>
   )
 }
