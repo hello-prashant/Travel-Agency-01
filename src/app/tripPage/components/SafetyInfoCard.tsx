4f27430aaa6915f51ccd9bcefcb7b1ecbe0430da
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-04 23:34:15 +0530

    style: update SafetyInfoCard styles for icons

--- src/app/tripPage/components/SafetyInfoCard.tsx
+++ src/app/tripPage/components/SafetyInfoCard.tsx
@@ -1,8 +1,8 @@
-import { ShieldCheck } from "lucide-react"
+import { LuShieldCheck } from "react-icons/lu";
 export default function SafetyInfoCard(){
   return (
-    <div className="w-full min-h-24 p-3 bg-blue-100 rounded-lg border border-green-300 inline-flex gap-2">
-      <ShieldCheck className='text-blue-100 fill-green-500' width={28} height={28}/>
+    <div className="w-full min-h-24 px-6 py-4 bg-blue-100 rounded-lg border border-green-300 inline-flex items-center gap-6 ">
+      <LuShieldCheck className='text-blue-100 fill-green-500 scale-250' />
       <div className="">
         <h4 className='text-lg font-semibold mb-3'>Safety Information</h4>
         <p className='text-gray-800'>All travelers are verified by the community. The trip leader has a 96% safety rating.</p>
