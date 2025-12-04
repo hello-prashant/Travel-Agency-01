99a6da080b7f47806f382357bb2bd0ba4e80b29a
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-04 20:13:13 +0530

    Add CancellationCard component and update TripPage to use it

---
+++ src/app/tripPage/components/CancellationCard.tsx
@@ -0,0 +1,8 @@
+export default function CancellationCard(){
+  return (
+    <div className="w-full min-h-24 p-3 bg-gray-100 rounded-lg border border-gray-400">
+      <h4 className='text-lg font-semibold mb-3'>Cancellation Policy</h4>
+      <p className='text-gray-800'>Free cancellation up to 7 days before trip start date.</p>
+    </div>
+  )
+}
--- src/app/tripPage/page.tsx
+++ src/app/tripPage/page.tsx
@@ -1,10 +1,10 @@
-import DetailedItineraryCard from "@/app/tripPage/components/DetailedItineraryCard";
+import CancellationCard from "./components/CancellationCard"
 
 export default function TripPage(){
   return (
     <section
     className="w-full min-h-svh p-6 flex flex-col gap-4">
-      <DetailedItineraryCard />
+      <CancellationCard />
     </section>
   )
 }
