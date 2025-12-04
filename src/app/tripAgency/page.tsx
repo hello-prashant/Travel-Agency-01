a473b25d5c9abf5ee55dd046f5c368e72a99fe4b
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-04 23:02:56 +0530

    feat: add HeroSection component and integrate into TripAgency page
    - Created HeroSection component with layout and styling for the hero section of the trip agency.
    - Included images, icons, and information such as title, location, ratings, and action buttons.
    - Integrated HeroSection into the TripAgency page for display.

---
+++ src/app/tripAgency/page.tsx
@@ -0,0 +1,10 @@
+import HeroSection from "./components/HeroSection"
+
+export default function TripAgency(){
+  return (
+    <section
+    className="w-full min-h-svh flex flex-col gap-4">
+      <HeroSection />
+    </section>
+  )
+}
