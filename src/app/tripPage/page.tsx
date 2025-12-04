5e7cf5f01ef37e498a92cb364b75401c95ac30d0
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-04 20:18:47 +0530

    Replace CancellationCard with SafetyInfoCard in TripPage component

--- src/app/tripPage/page.tsx
+++ src/app/tripPage/page.tsx
@@ -1,10 +1,10 @@
-import CancellationCard from "./components/CancellationCard"
+import SafetyInfoCard from "./components/SafetyInfoCard"
 
 export default function TripPage(){
   return (
     <section
     className="w-full min-h-svh p-6 flex flex-col gap-4">
-      <CancellationCard />
+      <SafetyInfoCard />
     </section>
   )
 }
