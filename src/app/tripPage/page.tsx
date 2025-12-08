1072eaca8915812f50b47580a3e10de2366ce4c9
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-08 16:31:02 +0530

    Add TripHighlightsCard component and integrate it into the trip page
    - Created TripHighlightsCard component to display trip highlights such as beach activities, local cuisine, temple visits, and yoga sessions.
    - Integrated TripHighlightsCard into the main trip page layout.

---
+++ src/app/tripPage/page.tsx
@@ -0,0 +1,12 @@
+import TripHighlightsCard from "./components/TripHighlightsCard";
+
+export default function page() {
+  return (
+    <div className="w-full min-h-svh p-6">
+      <div className="w-[70%]">
+
+      <TripHighlightsCard />
+      </div>
+    </div>
+  )
+}
