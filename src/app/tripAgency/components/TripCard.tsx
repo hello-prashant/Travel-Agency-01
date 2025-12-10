50740f77618f4a7f9b2445574c1b3d92bf23501a
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-10 15:24:20 +0530

    update Trip Card styles for joined member's image

--- src/app/tripAgency/components/TripCard.tsx
+++ src/app/tripAgency/components/TripCard.tsx
@@ -26,27 +26,32 @@ export default function TripCard({trip, past}:Props) {
           </span>
         </div>
 
-        <div className="flex flwx-row items-center relative w-full h-8 md:h-10">
-          {Array.isArray(trip.memberImgUrl) && trip.memberImgUrl.length > 0?
-            trip.memberImgUrl.map((src: string, idx: number)=>(
-            <img key={idx} src={src} alt="Traveler image" 
-              loading="lazy"
-              className={`w-8 md:w-10 h-full object-cover rounded-full border-2 border-white -translate-x-${idx*2}`}
-            />
-          )) :
-            <></>
-          }
-
-          <span className="inline-flex items-center gap-1 text-gray-600 text-sm"> 
-            {past ? `${trip.groupSize} travelers `: `${trip.joined}+'/'+${trip.groupSize}` } 
+        <div className="flex flwx-row items-center gap-4 w-full h-8 md:h-10">
+          <div className="inline-flex relative items-center h-full">
+            {Array.isArray(trip.memberImgUrl) && trip.memberImgUrl.length > 0?
+              trip.memberImgUrl.map((src: string, idx: number)=>(
+                <img key={idx} src={src} alt="Traveler image" 
+                  loading="lazy"
+                  className="w-8 md:w-10 h-full object-cover rounded-full border-2 border-white"
+                  style={{transform: `translateX(-${idx * 8}px)`}}
+                />
+              )) :
+              <></>
+            }
+          </div>
+          
+          <span className="text-gray-600 text-sm"> 
+            {past ? `${trip.groupSize} travelers `: `${trip.joined}/${trip.groupSize} ` } 
             joined
           </span>
         </div>
 
         <div className="w-full inline-flex items-center gap-4 ">
-          <button className="px-4 py-1.5 md:py-2 bg-cyan-800 text-white ">
-            Join Trip
-          </button>
+          {!past &&
+            <button className="px-4 py-1.5 md:py-2 bg-cyan-800 text-white ">
+              Join Trip
+            </button>
+          }
 
           <button className="px-4 py-1.5 md:py-2 text-cyan-800 border border-cyan-800">
             View trip details
