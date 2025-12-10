3192adf9c8e7c698d7dc5678580e711bff336d64
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-10 19:46:57 +0530

    Add Contact Information Card component and integrated into Trip Agency page

---
+++ src/app/tripAgency/page.tsx
@@ -0,0 +1,12 @@
+import ContactInfoCard from "./components/ContactInfoCard"
+
+export default function page() {
+  return (
+    <div className="w-full min-h-svh p-6">
+      <div className="w-[70%]">
+
+        <ContactInfoCard />
+      </div>
+    </div>
+  )
+}
