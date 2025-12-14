f658c78155eea2d6c34f7b6b7fb0c62a645ae3eb
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-14 20:22:19 +0530

    Add Similar Trips Card component

---
+++ src/app/tripPage/page.tsx
@@ -0,0 +1,8 @@
+import SimilarTripCard from "./components/SimilarTripCard"
+export default function TripAgency() {
+  return (
+    <div className="w-full min-h-svh p-6">
+      <SimilarTripCard />
+    </div>
+  )
+}
