2fdcd8d6f29acefb7950107b44bb884687e85d42
thakuranshu-dev <thakuranshu01485@gmail.com>
2025-12-15 17:06:30 +0530

    Add Travel Photos Cpmponent and integrated into Travel Agency page.

---
+++ src/app/tripAgency/page.tsx
@@ -0,0 +1,9 @@
+import TravelPhotosCard from "./components/TravelPhotosCard"
+
+export default function page() {
+  return (
+    <div className="p-6 w-1/2">
+      <TravelPhotosCard />
+    </div>
+  )
+}
